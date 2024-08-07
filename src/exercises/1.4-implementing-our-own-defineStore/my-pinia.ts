2e8a484c11687a09ecccb9e030844769d456b43e
Mohamed Fadl <mohamed.fadl.farag@gmail.com>
2024-08-07 10:56:08 +0300

    chore: Refactor defineStore to use effectScope and WeakMap

--- src/exercises/1.4-implementing-our-own-defineStore/my-pinia.ts
+++ src/exercises/1.4-implementing-our-own-defineStore/my-pinia.ts
@@ -1,3 +1,19 @@
-export function defineStore() {
-  // TODO: see instructions.md
+import { effectScope } from 'vue'
+
+const stores = new WeakMap<() => unknown, unknown>()
+
+const globalEffectScope = effectScope(true)
+
+export function defineStore<R>(fn: () => R) {
+  function useStore() {
+    if (!stores.has(fn)) {
+      const store = globalEffectScope.run(() => effectScope().run(fn))
+
+      stores.set(fn, store)
+    }
+
+    return stores.get(fn) as R
+  }
+
+  return useStore
 }
