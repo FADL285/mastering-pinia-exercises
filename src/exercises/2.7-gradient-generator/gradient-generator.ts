599ab67b4f44143703aeba00edd48932483be45f
Mohamed Fadl <mohamed.fadl.farag@gmail.com>
2024-08-15 09:47:46 +0300

    Solve Gradient Generator Exercise

--- src/exercises/2.7-gradient-generator/gradient-generator.ts
+++ src/exercises/2.7-gradient-generator/gradient-generator.ts
@@ -1,8 +1,51 @@
+import { useClipboard } from '@vueuse/core'
 import { acceptHMRUpdate, defineStore } from 'pinia'
-import { computed, ref, shallowRef } from 'vue'
+import { computed, ref } from 'vue'
+
+const DEFAULT_COLORS = ['#00c9ff', '#92fe9d']
+const DEFAULT_ANGLE = 90
 
 export const useGradientGenerator = defineStore('gradient-generator', () => {
-  // build out your composable here
+  const colors = ref<string[]>([...DEFAULT_COLORS])
+  const angle = ref(DEFAULT_ANGLE)
+
+  const gradient = computed(() => `linear-gradient(${angle.value}deg , ${colors.value.join(', ')})`)
+  const gradientCSS = computed(() => `background-image: ${gradient.value};`)
+
+  function addRandomColor() {
+    colors.value.push(randomColor())
+  }
+
+  function removeColor(index: number) {
+    if (colors.value.length <= 2 || index < 0 || index >= colors.value.length) return
+    colors.value.splice(index, 1)
+  }
+
+  const { copy } = useClipboard()
+  function copyToClipboard() {
+    copy(gradientCSS.value)
+  }
+
+  function randomize(steps = 2) {
+    colors.value = Array.from({ length: steps }, randomColor)
+  }
+
+  function $reset() {
+    colors.value = [...DEFAULT_COLORS]
+    angle.value = DEFAULT_ANGLE
+  }
+
+  return {
+    colors,
+    angle,
+    gradient,
+    gradientCSS,
+    addRandomColor,
+    removeColor,
+    copyToClipboard,
+    randomize,
+    $reset,
+  }
 })
 
 /**
--- src/exercises/2.7-gradient-generator/index.vue
+++ src/exercises/2.7-gradient-generator/index.vue
@@ -1,43 +1,68 @@
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+import { useGradientGenerator } from './gradient-generator'
+
+const gradient = useGradientGenerator()
+</script>
 
 <template>
   <h1 class="mb-5">Gradient Generator</h1>
 
   <div class="my-2 space-x-1">
-    <button data-test="btn-reset">Reset</button>
-    <button data-test="btn-randomize">Randomize</button>
-    <button data-test="btn-clipboard">Copy to Clipboard</button>
+    <button data-test="btn-reset" @click="gradient.$reset">Reset</button>
+    <button data-test="btn-randomize" @click="gradient.randomize(2 + Math.floor(Math.random() * 2))">Randomize</button>
+    <button data-test="btn-clipboard" @click="gradient.copyToClipboard">Copy to Clipboard</button>
   </div>
+
+  <p>
+    <strong>Gradient:</strong>
+    <code>{{ gradient.gradientCSS }}</code>
+  </p>
 
   <div class="rounded gradient-preview"></div>
 
   <div class="flex py-1 my-2">
     <label class="flex items-center">
       <span>Angle: </span>
-      <input data-test="angle-value-range" class="mx-3" type="range" step="1" min="0" max="360" />
+      <input
+        v-model="gradient.angle"
+        data-test="angle-value-range"
+        class="mx-3"
+        type="range"
+        step="1"
+        min="0"
+        max="360"
+      />
     </label>
-    <input data-test="angle-value-number" type="number" min="0" max="360" />
+    <input v-model="gradient.angle" data-test="angle-value-number" type="number" min="0" max="360" />
   </div>
 
   <div class="flex flex-wrap">
     <!-- Display a swatch of each color in the gradient -->
-    <div class="flex flex-col items-center group">
-      <input class="w-12 h-10 p-1 m-1" type="color" />
+    <div v-for="(_color, index) in gradient.colors" :key="index" class="flex flex-col items-center group">
+      <input v-model="gradient.colors[index]" class="w-12 h-10 p-1 m-1" type="color" />
 
       <!-- Should remove color when this button is clicked -->
-      <button class="invisible w-5 h-5 p-0 group-hover:visible" data-test="btn-remove-color" title="Remove this color">
+      <button
+        class="invisible w-5 h-5 p-0 group-hover:visible"
+        data-test="btn-remove-color"
+        title="Remove this color"
+        :disabled="gradient.colors.length <= 2"
+        @click="gradient.removeColor(index)"
+      >
         -
       </button>
     </div>
 
-    <button class="w-12 h-10 p-1 m-1" data-test="btn-add-color" title="Add Color">+</button>
+    <button class="w-12 h-10 p-1 m-1" data-test="btn-add-color" title="Add Color" @click="gradient.addRandomColor">
+      +
+    </button>
   </div>
 </template>
 
 <style scoped>
 .gradient-preview {
   aspect-ratio: 2 / 1;
-  background-image: linear-gradient(#00c9ff, #92fe9d);
+  background-image: v-bind('gradient.gradient');
   min-height: 75px;
 }
 </style>
