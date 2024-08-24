c37749687d96ae5162bbec31baa96b4b7211cd4e
Mohamed Fadl <mohamed.fadl.farag@gmail.com>
2024-08-24 12:24:19 +0300

    Finish 6.5.4-readonly-state Exercise 🔥

--- src/exercises/6.5.4-readonly-state/readonly-state.ts
+++ src/exercises/6.5.4-readonly-state/readonly-state.ts
@@ -1,14 +1,36 @@
 // NOTE: remove this line (or change the 1 into 0) if you don't want
 // to work on the Type Safety part
-/* eslint @typescript-eslint/no-explicit-any:1 */
-import { defineStore } from 'pinia'
-import { reactive } from 'vue'
+/* eslint @typescript-eslint/no-explicit-any:2 */
+import { defineStore, StateTree } from 'pinia'
+import { computed, ComputedRef, UnwrapRef } from 'vue'
 
-export function defineReadonlyState(id: any, privateStateFn: any, setup: any) {
+export function defineReadonlyState<Id extends string, PrivateState extends StateTree, SetupReturn>(
+  id: Id,
+  privateStateFn: () => PrivateState,
+  setup: (privateSTate: PrivateState) => SetupReturn,
+) {
+  const usePrivateState = defineStore(id + '_private', {
+    state: privateStateFn,
+  })
+
   return defineStore(id, () => {
-    return setup(
-      // FIXME: you will have to rewrite this to make it work
-      reactive({ n: 0 }),
-    )
+    const privateState = usePrivateState()
+    const result = setup(privateState.$state as PrivateState)
+
+    // Initialize an empty object with the correct type
+    const privateStateAsGetters: Record<
+      keyof UnwrapRef<PrivateState>,
+      ComputedRef<UnwrapRef<PrivateState>[keyof UnwrapRef<PrivateState>]>
+    > = {} as Record<keyof UnwrapRef<PrivateState>, ComputedRef<UnwrapRef<PrivateState>[keyof UnwrapRef<PrivateState>]>>
+
+    for (const key in privateState.$state) {
+      const typedKey = key as keyof UnwrapRef<PrivateState>
+      privateStateAsGetters[typedKey] = computed(() => privateState.$state[typedKey])
+    }
+
+    return {
+      ...privateStateAsGetters,
+      ...result,
+    }
   })
 }
